7474e90fddb3096135283a9221261894d6a58feb
Gennadii <Ligr98@gmail.com>
2021-01-01 16:50:21 +0300

    Now all photos from page can be downloaded

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import config
 import requests
 from pprint import pprint
 from datetime import datetime
-import time
-
-
-with open('token.txt', 'r') as file_object:
-    token_vk = file_object.read().strip()
+import json
 
 
 class VkUser:
@@ -37,6 +34,18 @@ class VkUser:
         response_photo_vk = requests.get(photos_url, params={**self.params, **photos_params})
         print('Info about photos was loaded from vk')
         return response_photo_vk.json()
+
+    def get_all_photos(self, user_id=None):
+        if user_id is None:
+            user_id = self.owner_id
+        all_photos_url = self.url + 'photos.getAll'
+        all_photos_params = {
+            'owner_id': user_id,
+            'extended': '1',
+        }
+        response_all_photos_vk = requests.get(all_photos_url, params={**self.params, **all_photos_params})
+        print('Info about all photos was loaded from vk')
+        return response_all_photos_vk.json()
 
     def change_vk_response(self, response_vk):
         ''' Changing vk_response to type of dictionary that we need:
@@ -89,8 +98,8 @@ class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
-    def make_a_folder(self):
-        HEADERS = {"Authorization": "OAuth "}
+    def make_a_folder_photos(self):
+        HEADERS = {"Authorization": f"OAuth {self.token}"}
 
         response_folder = requests.put("https://cloud-api.yandex.net/v1/disk/resources",
                                        params={"path": f"/id{photos_from_vk[0]['owner_id']}_{number_of_photos}"},
@@ -99,11 +108,24 @@ class YaUploader:
         result_folder = response_folder.json()
         href_ya = result_folder['href']
         # print(href_ya)
-        print('Folder on Yandex disk was created')
+        print('Folder on Yandex disk for photos was created')
         return href_ya
 
+    def make_a_folder_json(self):
+        HEADERS = {"Authorization": f"OAuth {self.token}"}
+
+        response_folder = requests.put("https://cloud-api.yandex.net/v1/disk/resources",
+                                       params={"path": f"/id{photos_from_vk[0]['owner_id']}_{number_of_photos}/output"},
+                                       headers=HEADERS)
+        response_folder.raise_for_status()
+        result_folder = response_folder.json()
+        href_ya_json = result_folder['href']
+        # print(href_ya)
+        print('Folder on Yandex disk for json-file was created')
+        return href_ya_json
+
     def upload_by_url(self):
-        HEADERS = {"Authorization": "OAuth "}
+        HEADERS = {"Authorization": f"OAuth {self.token}"}
         for n in range(number_of_photos):
             # print(len(photos_from_vk))
             # print(n)
@@ -119,6 +141,27 @@ class YaUploader:
             print(f'{n + 1} pictures loaded')
         print('Photos from vk were uploaded to Yandex disk')
 
+    def upload_json(self, file_path: str):
+        """Метод загруджает файл file_path на яндекс диск"""
+        HEADERS = {"Authorization": f"OAuth {self.token}"}
+
+        resp1 = requests.get("https://cloud-api.yandex.net/v1/disk/resources/upload",
+                             params={"path": f"/id{photos_from_vk[0]['owner_id']}_{number_of_photos}/output/upload.json",
+                                     "overwrite": "true"},
+                             headers=HEADERS)
+
+        resp1.raise_for_status()
+        result1 = resp1.json()
+        # pprint(result1)
+        href = result1["href"]
+
+        with open(file_path, "r") as f:
+            resp2 = requests.put(href, files={"file": f})
+
+        resp2.raise_for_status()
+
+        return print('Json was successfully uploaded')
+
 
 def main():
     global number_of_photos
@@ -126,15 +169,36 @@ def main():
     number_of_photos = 5
     id_vk_input = int(input('Введите id пользователя vk: '))
     ya_token_input = input('Введите яндекс-токен: ')
-    number_of_pic_input = input('Введите количество фотографий (по умолачанию скачаются последние 5): ')
+    number_of_pic_input = input('Введите количество фотографий (по умолчанию скачаются последние 5): ')
+    type_of_photos = input('Скачать фотографии только с профиля(1) или все(2)? Введите 1 или 2: ')
+
+    # id_vk_input = 79932267
+    # ya_token_input = "AgAAAABGl0B0AADLW4oimzzNWkOThPSoLoSCJ9o"
+    # number_of_pic_input = input('Введите количество фотографий (по умолчанию скачаются последние 5): ')
+
     if number_of_pic_input != '':
         number_of_photos = int(number_of_pic_input)
-    vk_client = VkUser(token_vk, '5.126', id_vk_input)
-    result_vk = VkUser.get_photos(vk_client)
+    vk_client = VkUser(config.vk_token, '5.126', id_vk_input)
+
+    if type_of_photos == '1':
+        result_vk = VkUser.get_photos(vk_client)
+    elif type_of_photos == '2':
+        result_vk = VkUser.get_all_photos(vk_client)
+    else:
+        print("Введённые данные не верны")
+
+    with open("output.json", 'w') as json_file:
+        output = json.dumps(result_vk, sort_keys=True, indent=4)
+        json_file.write(output)
+
     photos_from_vk = VkUser.change_vk_response(vk_client, result_vk)
     ya_client = YaUploader(ya_token_input)
-    href_for_ya = ya_client.make_a_folder()
+    href_for_ya = ya_client.make_a_folder_photos()
+    ya_client.make_a_folder_json()
+    ya_client.upload_json("output.json")
     ya_client.upload_by_url()
+
+
     # print()
     # pprint(result_vk)
 
@@ -142,56 +206,5 @@ def main():
 main()
 
 # тестовый id vk - 552934290
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Потом это удалю
-# vk_client1 = VkUser(token_vk, '5.126', 552934290)
-# vk_client2 = VkUser(token_vk, '5.126', 79932267)
-# vk_client3 = VkUser(token_vk, '5.126', 144348760)
-
-# pprint(vk_client1.get_photos())
-# pprint(VkUser.get_photos(vk_client1))
-# result = vk_client3.get_photos()
-
-# pprint(VkUser.get_photos(vk_client3))
-# result1 = VkUser.get_photos(vk_client3)
-# pprint(vk_client3.change_vk_response(result))
-
-# vk_client3.change_vk_response(result)
-# pprint(VkUser.change_vk_response(vk_client3, result1))
-
-# photos_from_vk = VkUser.change_vk_response(vk_client3, result1)
-
-# print(photos_from_vk[0]['name'])
-# print(photos_from_vk[0]['url_max_size'])
-
-
-# number_of_photos = 4
-#
-# ya_client = YaUploader("AgAAAABGl0B0AADLW4oimzzNWkOThPSoLoSCJ9o")
-# href_for_ya = ya_client.make_a_folder()
-# ya_client.upload_by_url()
-
-
-
-
-
-
-
 
 
